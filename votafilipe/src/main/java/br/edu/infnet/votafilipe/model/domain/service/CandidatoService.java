4b727a87ddcd6f25f6e09df0551502e3062bc1b6
Filipe Rangel da Costa <filipe.rcosta@al.infnet.edu.br>
2022-05-28 15:47:31 -0300

    Second comment

--- votafilipe/src/main/java/br/edu/infnet/votafilipe/model/domain/service/CandidatoService.java
+++ votafilipe/src/main/java/br/edu/infnet/votafilipe/model/domain/service/CandidatoService.java
@@ -3,6 +3,8 @@ package br.edu.infnet.votafilipe.model.domain.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.edu.infnet.votafilipe.model.domain.Candidato;
 import br.edu.infnet.votafilipe.model.domain.Eleicao;
@@ -14,6 +16,13 @@ public class CandidatoService {
 	
 	public void incluir(Candidato candidato) {
 		candidatoRepository.save(candidato);
+	}
+	
+	@GetMapping(value = "/candidato/{id}/excluir")
+	public String excluir(@PathVariable Integer id) {
+		//candidatoService.excluir(id);
+		
+		return "redirect:/candidatos";
 	}
 	
 	public List<Candidato> obterLista(){
